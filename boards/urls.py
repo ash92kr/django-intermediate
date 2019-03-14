2e5891b3cae43d0ee32556479efc280b2b9e4ff1
ash92kr <ash92kr@gmail.com>
2019-03-14 01:40:35 +0000

    00_RESTful URL setting

--- boards/urls.py
+++ boards/urls.py
@@ -4,13 +4,13 @@ from . import views
 app_name = 'boards'
 
 urlpatterns = [
-    path('<int:pk>/update/', views.update, name='update'),
-    path('<int:pk>/edit/', views.edit, name='edit'),
-    path('<int:pk>/delete/', views.delete, name='delete'),
+    # path('<int:pk>/update/', views.update, name='update'),
+    path('<int:pk>/edit/', views.edit, name='edit'),  # GET 방식은 edit, POST 방식은 update
+    path('<int:pk>/delete/', views.delete, name='delete'),  # POST일 때만 delete 작동하기
     path('<int:pk>/', views.detail, name='detail'),
-    path('create/', views.create, name='create'),
-    path('new/', views.new, name='new'),
-    path('', views.index, name='index'),
+    # path('create/', views.create, name='create'),
+    path('new/', views.new, name='new'),  # GET으로 들어오면 NEW, POST로 들어오면 CREATE -> if문 사용
+    path('', views.index, name='index'),  # GET으로 들어오면 form을 보여주고 POST로 들어오면 정보를 저장한다
 ]
 
 
--- boards/views.py
+++ boards/views.py
@@ -1,25 +1,45 @@
 from django.shortcuts import render, redirect
 from .models import Board
+from pprint import pprint   # 터미널에서 볼 때 사용
 
 # Create your views here.
 def index(request):
+    pprint(request)
+    pprint(type(request))
+    pprint(dir(request))
+    pprint(request.scheme)
+    pprint(request.get_host())
+    pprint(request.get_full_path())
+    pprint(request.build_absolute_uri())
+    pprint(request.META)
+    pprint(request.method)
+    
     boards = Board.objects.order_by('-pk')
     context = {  # context를 통해 딕셔너리를 html에 넣음
         'boards': boards,
     }
     return render(request, 'boards/index.html', context)
     
-def new(request):
-    return render(request, 'boards/new.html')
+def new(request):  # 함수가 하나 줄어들고 restful하게 바뀜
+    if request.method == 'POST':
+        # CREATE
+        title = request.POST.get('title')
+        content = request.POST.get('content')
+        
+        board = Board(title=title, content=content)
+        board.save()
+        return redirect('boards:detail', board.pk)
+    else:   
+        return render(request, 'boards/new.html')
     
-def create(request):
-    title = request.POST.get('title')
-    content = request.POST.get('content')
+# def create(request):
+#     title = request.POST.get('title')
+#     content = request.POST.get('content')
     
-    board = Board(title=title, content=content)
-    board.save()
-    # return redirect(f'/boards/{ board.pk }/')
-    return redirect('boards:detail', board.pk)
+#     board = Board(title=title, content=content)
+#     board.save()
+#     # return redirect(f'/boards/{ board.pk }/')
+#     return redirect('boards:detail', board.pk)
     
 def detail(request, pk):
     board = Board.objects.get(pk=pk)
@@ -30,22 +50,32 @@ def detail(request, pk):
     
 def delete(request, pk):
     board = Board.objects.get(pk=pk)
-    board.delete()
-    # return redirect('/boards/')
-    return redirect('boards:index')
+    if request.method == "POST":
+        board.delete()
+        # return redirect('/boards/')
+        return redirect('boards:index')
+    else:  # 주소로 입력하면 detail 페이지가 보이도록 함
+        return redirect('boards:detail', board.pk)
     
 def edit(request, pk):
     board = Board.objects.get(pk=pk)
-    context = {
-        'board': board,
-    }
-    return render(request, 'boards/edit.html', context)
+    
+    if request.method == "POST":  # UPDATE
+        board.title = request.POST.get('title')
+        board.content = request.POST.get('content')
+        board.save()
+        return redirect('boards:detail', board.pk)
+    else:   # EDIT
+        context = {
+            'board': board,
+        }
+        return render(request, 'boards/edit.html', context)
 
-def update(request, pk):
-    board = Board.objects.get(pk=pk)
-    board.title = request.POST.get('title')
-    board.content = request.POST.get('content')
-    board.save()
-    # return redirect(f'/boards/{ board.pk }/')
-    return redirect('boards:detail', board.pk)
+# def update(request, pk):
+#     board = Board.objects.get(pk=pk)
+#     board.title = request.POST.get('title')
+#     board.content = request.POST.get('content')
+#     board.save()
+#     # return redirect(f'/boards/{ board.pk }/')
+#     return redirect('boards:detail', board.pk)
     
