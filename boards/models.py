72d779eef3013277b3ba7f600e0ab9b470a5d087
ash92kr <ash92kr@gmail.com>
2019-03-14 09:19:47 +0000

    01_1:N_crud

--- boards/models.py
+++ boards/models.py
@@ -10,3 +10,13 @@ class Board(models.Model):
     def __str__(self):   # 이 부분도 자동으로 입력하기
         return f"{self.id}: {self.title}"
         # return self.title
+        
+
+class Comment(models.Model):   # 외래키 속성은 어디에 두든지 간에 가장 마지막 속성에 붙는다
+    board = models.ForeignKey(Board, on_delete=models.CASCADE)   # Board 테이블 참조하며, 
+    content = models.CharField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.content
--- boards/views.py
+++ boards/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from .models import Board
+from .models import Board, Comment
 from pprint import pprint   # 터미널에서 볼 때 사용
 
 # Create your views here.
@@ -41,15 +41,17 @@ def new(request):  # 함수가 하나 줄어들고 restful하게 바뀜
 #     # return redirect(f'/boards/{ board.pk }/')
 #     return redirect('boards:detail', board.pk)
     
-def detail(request, pk):
-    board = Board.objects.get(pk=pk)
+def detail(request, board_pk):
+    board = Board.objects.get(pk=board_pk)
+    comments = board.comment_set.all()
     context = {
         'board': board,
+        'comments': comments,
     }
     return render(request, 'boards/detail.html', context)
     
-def delete(request, pk):
-    board = Board.objects.get(pk=pk)
+def delete(request, board_pk):
+    board = Board.objects.get(pk=board_pk)
     if request.method == "POST":
         board.delete()
         # return redirect('/boards/')
@@ -57,8 +59,8 @@ def delete(request, pk):
     else:  # 주소로 입력하면 detail 페이지가 보이도록 함
         return redirect('boards:detail', board.pk)
     
-def edit(request, pk):
-    board = Board.objects.get(pk=pk)
+def edit(request, board_pk):
+    board = Board.objects.get(pk=board_pk)
     
     if request.method == "POST":  # UPDATE
         board.title = request.POST.get('title')
@@ -79,3 +81,24 @@ def edit(request, pk):
 #     # return redirect(f'/boards/{ board.pk }/')
 #     return redirect('boards:detail', board.pk)
     
+def comments_create(request, board_pk):
+    # 댓글을 달 게시물부터 찾기
+    board = Board.objects.get(pk=board_pk)   # board의 pk와 comment의 board_pk가 일치하는 board를 가져옴
+    
+    # form에서 넘어온 comment data
+    content = request.POST.get('content')
+    
+    # 댓글 생성 및 저장
+    comment = Comment(board=board, content=content)   # Comment의 board에 board 객체 전체를 넣으면, 필요한 부분만 가져감
+    comment.save()
+    return redirect('boards:detail', board.pk)
+        
+        # 몇 번 게시글의 몇 번 댓글을 지워야 하는가?
+def comments_delete(request, board_pk, comment_pk):
+    comment = Comment.objects.get(pk=comment_pk)
+    
+    if request.method == 'POST':
+        comment.delete()
+        return redirect('boards:detail', board_pk)   # 현재 게시글을 보여줌
+    else:
+        return redirect('boards:detail', board_pk)
